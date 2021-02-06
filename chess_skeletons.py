a8aa32beff6c90115f38bd8e11d693293257b8f8
David <dlruddell@gmail.com>
2021-02-06 16:30:02 -0500

    added board init functionality and __str__, __repr__ to obs except player obs

--- chess_skeletons.py
+++ chess_skeletons.py
@@ -3,11 +3,12 @@
 class ChessBoard(object):
     """
     """
-    def __init__(self, pieces=None, dead_pieces=None, size=(8, 8), mode="default"):
+    def __init__(self, size=(8, 8), board=None, pieces=None, dead_pieces=None, mode="default"):
         """
         """
-        self._pieces = pieces
+        self._board = board
         self._size = size # attributes retained by the class need the self reference also
+        self._pieces = pieces
         self._dead_pieces = dead_pieces
 
         self._generate_board_state(mode=mode) # using functions for initialization can be good practice
@@ -16,25 +17,90 @@ class ChessBoard(object):
         return self._size # how to access class attributes some where else in the object
 
     def _generate_board_state(self, mode="default"):
+        self._board = []
+        for rank in range(self._size[0]):
+            self._board.append([])
+            for file_ in range(self._size[1]):
+                if (rank + file_) % 2 == 0:
+                    self._board[rank].append(Square('black'))
+                else:
+                    self._board[rank].append(Square('white'))
+
         if mode == "default":
             self._generate_default_board() # calling functions inside others can make your code more concise and clean
         elif mode == "random":
             self._generate_random_board()
 
     def _generate_default_board(self):
-        pass
+        self._pieces = {'white': [],
+                        'black': []}
+        self._dead_pieces = {'white': [],
+                             'black': []}
+        power_pieces = {0: Rook, 1: Knight, 2: Bishop, 3: Queen,
+                        4: King, 5: Bishop, 6: Knight, 7: Rook}
+        for rank in range(self._size[0]):
+            for file_ in range(self._size[1]):
+                if rank == 1:
+                    color = 'white'
+                    piece = Pawn(position=(rank, file_), color=color)
+                elif rank == 6:
+                    color = 'black'
+                    piece = Pawn(position=(rank, file_), color=color)
+                elif rank == 0:
+                    color = 'white'
+                    piece = power_pieces[file_](position=(rank, file_), color=color)
+                elif rank == 7:
+                    color = 'black'
+                    piece = power_pieces[file_](position=(rank, file_), color=color)
+                else:
+                    piece = None
+                if piece != None:
+                    self._pieces[color].append(piece)
+
+                self._board[rank][file_]._piece = piece
+
+    def __repr__(self, view='white'):
+        repr_string = ""
+        if view == 'white':
+            for rank in range(len(self._board)-1, -1, -1):
+                repr_string += str(self._board[rank]) + '\n'
+        elif view == 'black':
+            for rank in range(len(self._board)):
+                repr_string += str(self._board[rank][::-1]) + '\n'
+        return repr_string
+
+    def __str__(self):
+        return self.__repr__()
+
+    def _visualize(self, view='white'):
+        return self.__repr__(view=view)
+
+
+
+class Square(object):
+    def __init__(self, color, piece=None):
+        self._color = color
+        self._piece = None
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        if self._piece == None:
+            return self._color[0]
+        return str(self._piece)
 
 
 class Piece(object):    # truth be told, I don't know what (object) does because they all should inherit from object
     """
     """
-    def __init__(self, position, player, color, board): # abstract definition that can be called in child class
+    def __init__(self, position, color, player=None, ptype='Piece'): # abstract definition that can be called in child class
         """
         """
         self._position = position
         self._player = player
         self._color = color
-        self._board = board
+        self._ptype = ptype
 
     def _move(self):    # abstract definition to be overloaded
         pass
@@ -48,59 +114,71 @@ class Piece(object):    # truth be told, I don't know what (object) does because
     def _get_moves(self):
         pass
 
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self._ptype[0]
+
 
 class Pawn(Piece):
     """
     """
-    def __init__(self, position):
+    def __init__(self, position, color, player=None):
         """
         """
-        super(Pawn, self).__init__(position=position)    # calling parent init function
+        super(Pawn, self).__init__(position=position, color=color, player=player,
+            ptype='Pawn')    # calling parent init function
 
 
 class Knight(Piece):
     """
     """
-    def __init__(self, position):
+    def __init__(self, position, color, player=None):
         """
         """
-        super(Knight, self).__init__(position=position)   # this also give an instance the self._position attribute
+        super(Knight, self).__init__(position=position, color=color, player=player,
+            ptype='knight')   # this also give an instance the self._position attribute
 
 
 class Bishop(Piece):
     """
     """
-    def __init__(self, position):
+    def __init__(self, position, color, player=None):
         """
         """
-        super(Bishop, self).__init__(position=position)
+        super(Bishop, self).__init__(position=position, color=color, player=player,
+            ptype='Bishop')
 
 
 class Rook(Piece):
     """
     """
-    def __init__(self, position):
+    def __init__(self, position, color, player=None):
         """
         """
-        super(Rook, self).__init__(position=position)
+        super(Rook, self).__init__(position=position, color=color, player=player,
+            ptype='Rook')
 
 
 class Queen(Piece):
     """
     """
-    def __init__(self, position):
+    def __init__(self, position, color, player=None):
         """
         """
-        super(Queen, self).__init__(position=position)
+        super(Queen, self).__init__(position=position, color=color, player=player,
+            ptype='Queen')
 
 
 class King(Piece):
     """
     """
-    def __init__(self, position):
+    def __init__(self, position, color, player=None):
         """
         """
-        super(King, self).__init__(position=position)
+        super(King, self).__init__(position=position, color=color, player=player,
+            ptype='King')
 
 
 class Player(object):
@@ -132,5 +210,5 @@ class AgentPlayer(object):
 
 if __name__ == "__main__": # this code only runs if you run this file (python chess_skeletons.py)
     chess_board0 = ChessBoard()             # instantiates a chess board
-    chess_board1 = ChessBoard((8,8))        # instantiates one just like it
-    chess_board1 = ChessBoard(size=(8,8))   # instantiates one also just like it
+    print(chess_board0)
+    piece = Pawn((0, 1), "white")
